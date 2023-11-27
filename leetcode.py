9d3eaffe96cf85e68517f1ab310e3d32f32a246a
Dung Dinh <dungdinh@Dungs-MacBook-Pro.local>
2023-11-27 15:58:09 +0800

    3sum

--- leetcode.py
+++ leetcode.py
@@ -11,7 +11,7 @@ Created on Mon Sep 18 20:02:47 2023
 # E.g: arr=[-1,2,3,4,1,-2], target = 0
 # Answer: [0,4] or [1,5]
 
-def returnIndicesofTarget(arr, target):
+def sum_two_numbers(arr, target):
     # for i in range(0, len(arr)):
     #     for j in range(1, len(arr)):
     #         if arr[i] + arr[j] == target:
@@ -27,4 +27,51 @@ def returnIndicesofTarget(arr, target):
             return [arr.index(num), find.index(num)]
         
         
-print(returnIndicesofTarget([-1,2,3,4,1,-2], 0))
+# print(sum_two_numbers([-1,2,3,4,1,-2], 0))
+
+#3 SUM
+def sum_three_numbers(arr, target):
+    i = 0
+    j = i + 1
+    
+    arr_len = len(arr)
+    
+    two_num_list = []
+    
+    while i < arr_len-1:
+        while j <arr_len:
+            two_num_list.append([i, j, arr[i]+arr[j]])
+            j = j+1
+        i = i+1
+        
+            
+    for index, value in enumerate(arr):
+        for j in two_num_list:
+            if target - j[2] == value:
+                if (index != j[1]) and (index != j[0]): 
+                    return [index, j[0], j[1]]
+
+# print(sum_three_numbers([0, 0, 0, 5, 3], 0))  
+# print(sum_three_numbers([1, -1, 2, 5, 3], 8))  
+
+def sum_by_2_pointers(arr, target):
+    arr.sort()
+    #[-1, 1, 2, 3, 5]
+    
+    for index, value in enumerate(arr):
+        s = index + 1
+        e = len(arr) - 1
+        
+        while s < e:
+            if (target - arr[s] - arr[e]) == 0:
+                return [index, s, e]
+            elif (target - arr[s] - arr[e]) > 0:
+                s = s+1
+            else:
+                e = e -1
+            
+    
+
+# print(sum_by_2_pointers([0, 0, 0, 5, 3], 0))
+print(sum_by_2_pointers([1, -1, 2, 5, 3], 8))
+    
